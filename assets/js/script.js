24bd8e29c2f099063f11b2b9202f86a44e16b516
PUTRI SEKAR AYU <222112294@student.stis.ac.id>
2025-06-07 18:09:01 +0700

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+const navToggleEvent = function (elem) {
+  for (let i = 0; i < elem.length; i++) {
+    elem[i].addEventListener("click", function () {
+      navbar.classList.toggle("active");
+      overlay.classList.toggle("active");
+    });
+  }
+}
+
+navToggleEvent(navElemArr);
+navToggleEvent(navLinks);
+
+
+
+/**
+ * header sticky & go to top
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 120) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+
+});
+
+function showSentimentResult() {
+  const input = document.getElementById("sentimentInput").value;
+  document.getElementById("resultSentence").textContent = input;
+  document.getElementById("sentimentModal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("sentimentModal").style.display = "none";
+}
